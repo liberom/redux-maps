0dbe3c4890dc5a2e4ed2fcfdcf1e691ea689cd6c
Vinicius Libero <libero.moreira@gmail.com>
2021-03-23 19:05:00 -0300

    updated map container

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { connect } from 'react-redux';
 
 // import Marker from '../components/marker.jsx';
 
@@ -9,6 +10,18 @@ class SimpleMap extends Component {
   render() {
     let marker = null;
     let center = { lat: 48.85, lng: 2.35 }
+
+    if (this.props.selectedFlat) {
+      marker = <div style={{width: '20px', height: '20px', backgroundColor: 'red', borderRadius: '50%' }}
+      lat={this.props.selectedFlat.lat}
+      lng={this.props.selectedFlat.lng} />;
+
+      center = {
+        lat: this.props.selectedFlat.lat,
+        lng: this.props.selectedFlat.lng
+      };
+    }
+
     return (
       // Important! Always set the container height explicitly
       <div className="col-sm-5" style={{ height: '100vh', width: '100%' }}>
@@ -27,4 +40,8 @@ class SimpleMap extends Component {
   }
 }
 
-export default SimpleMap;
+function mapStateToProps(state) {
+  return { selectedFlat: state.selectedFlat };
+}
+
+export default connect(mapStateToProps)(SimpleMap);
