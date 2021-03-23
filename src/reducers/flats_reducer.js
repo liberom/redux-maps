8a8426c4f78e61c4167fc3254ced6f03055a434f
Vinicius Libero <libero.moreira@gmail.com>
2021-03-23 17:43:01 -0300

    created flats_reducer

---
+++ src/reducers/flats_reducer.js
@@ -0,0 +1,17 @@
+// No need to name the reducer function
+
+export default function(state, action) {
+  // Goal: Compute and return a new state
+
+  if (state === undefined) {
+    // Reducer initialization
+    return [];
+  }
+  // handle some action
+
+};
+
+
+// OR:
+// const flatsReducer = (state, action) => {}
+// export default flatsReducer;
