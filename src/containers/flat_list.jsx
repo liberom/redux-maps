593a4df47f134b6e0baf3c584cdf7d3d8d793346
Vinicius Libero <libero.moreira@gmail.com>
2021-03-23 16:41:46 -0300

    included flat in flat_list

--- src/containers/flat_list.jsx
+++ src/containers/flat_list.jsx
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 
-import Flat from './flat.jsx';
+import Flat from '../components/flat.jsx';
 
 class FlatList extends Component {
+  // Temp code
+  static defaultProps = {
+    flats: [{
+      "name": "Charm at the Steps of Montmartre",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+      "price": 164,
+      "priceCurrency": "EUR"
+    }]
+  }
+
   render() {
     return (
       <div className="flat-list col-sm-7" >
-
+        {this.props.flats.map((flat) => {
+          return <Flat key={flat.name} flat={flat} />;
+        })}
       </div>
     );
   }
